63c21a0d51451694a4b911133d3542adde243e0f
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-07-21 23:32:23 +0000

    fix 5

--- models/base_model.py
+++ models/base_model.py
@@ -47,4 +47,5 @@ class BaseModel:
         """
         str_repr = "[{}] ({}) {}".format(__class__.__name__,
                                          self.id, self.__dict__)
+        print(str_repr)
         return str_repr

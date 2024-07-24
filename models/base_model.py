ccc860af5f2cd2f86e36ab3fc274c99b429c0520
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-07-24 20:12:16 +0000

    save and reload

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ class BaseModel:
         this base class imports json, uuid and datetime
         to make unique id
     """
+
     time_format = '%Y-%m-%dT%H:%M:%S.%f'
 
     def __init__(self, *args, **kwargs):
@@ -19,31 +20,43 @@ class BaseModel:
                 created_at: using datetime module to set the date created
                 updated_at: sets the time the instance was last updated
         """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-        else:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
             self.id = kwargs['id']
             self.created_at = datetime.strptime(kwargs['created_at'],
                                                 BaseModel().time_format)
             self.updated_at = datetime.strptime(kwargs['updated_at'],
                                                 BaseModel().time_format)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            from models import storage
+            storage.new(self)
+
+    @classmethod
+    def retrieve(cls, obj_dict):
+        """ this class method remakes the class method """
+        return cls(obj_dict)
 
     def save(self):
         """ The save func updates the datetime
             value of the instance variable updated_at
         """
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ to_dict returns a dict representation
             of an instance.
         """
-        dict_repr = {'__class__': self.__class__.__name__}
+        dict_repr = {'__class__' : self.__class__.__name__}
         dict_repr.update(self.__dict__)
-        dict_repr['created_at'] = self.created_at.isoformat()
-        dict_repr['updated_at'] = self.updated_at.isoformat()
+        dict_repr.update({'created_at': self.created_at.isoformat()})
+        dict_repr.update({'updated_at': self.updated_at.isoformat()})
         return dict_repr
 
     def __str__(self):
@@ -52,6 +65,5 @@ class BaseModel:
         """
         dict_rep = self.__dict__
         cls = self.__class__.__name__
-        str_rep = "[{:s}] ({:s}) {}".format(cls,
-                                        self.id, dict_rep)
+        str_rep = "[{:s}] ({:s}) {}".format(cls, self.id, dict_rep)
         return str_rep
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 from models.base_model import BaseModel
 """ FileStorage Class """
 
@@ -8,3 +9,47 @@ class FileStorage(BaseModel):
         saving the diffrent instances of each class to their
         respective json files
     """
+    def __init__(self, file_path='file.json', obj={}):
+        """ Initializes the class for storing of information
+            Args:
+                __file_path: path to JSON file
+                __objects: dictionary of objects
+                            the keys are in format "<class_name>.id"
+        """
+        self.__file_path = file_path
+        self.__objects = obj
+
+    def all(self):
+        """ returns the dictionary __objects """
+        return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with
+            key <obj class name>.id
+        """
+        format_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+        self.__objects.update({format_key: obj})
+
+    def save(self):
+        """ serializes __objects to the JSON file
+            (path: __file_path)
+        """
+        serial_obj = {}
+        with open(self.__file_path, 'w') as savefile:
+            for key, value in self.__objects.items():
+                serial_obj[key] = value.to_dict()
+            serial = json.dumps(serial_obj, indent=4)
+            savefile.write(serial)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects """
+        deserial_obj = {}
+        try:
+            with open(self.__file_path, 'r') as jfile:
+                contents = jfile.read()
+                deserial = json.loads(contents)
+                for key, value in deserial.items():
+                    deserial_obj[key] = BaseModel(**value)
+                self.__objects = deserial_obj
+        except:
+            print()

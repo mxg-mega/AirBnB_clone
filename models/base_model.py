9b6935dbc775263c690dbeb18fda853ac7c82c1c
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-07-26 12:57:33 +0000

    fix 10

--- models/base_model.py
+++ models/base_model.py
@@ -25,11 +25,22 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-            self.id = kwargs['id']
-            self.created_at = datetime.strptime(kwargs['created_at'],
-                                                BaseModel().time_format)
-            self.updated_at = datetime.strptime(kwargs['updated_at'],
-                                                BaseModel().time_format)
+            if kwargs['id'] is not None:
+                self.id = kwargs['id']
+            else:
+                self.id = str(uuid.uuid4())
+
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                                                    BaseModel().time_format)
+            else:
+                self.created_at = datetime.now()
+
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                    BaseModel().time_format)
+            else:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()

f1c28c1650763841d96cfface6ddf4b1b1bb1ff8
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-07-27 22:13:33 +0000

    trying to pass the save test fix 4

--- models/base_model.py
+++ models/base_model.py
@@ -33,24 +33,24 @@ class BaseModel:
                 self.created_at = datetime.strptime(kwargs["created_at"],
                                                     date_time_format)
             else:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.now()
 
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"],
                                                     date_time_format)
             else:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def save(self):
         """ The save func updates the datetime
             value of the instance variable updated_at
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):

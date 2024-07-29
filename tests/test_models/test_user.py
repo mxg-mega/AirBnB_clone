40a2dcd053330b7a7ce71bc461f7c387079a5e0d
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-07-29 16:04:02 +0000

    test for the User class public attr

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,6 +12,16 @@ class TestUser(unittest.TestCase):
     def test_classname(self):
         self.assertEqual(self.a.to_dict()['__class__'], 'User')
 
+    def test_email(self):
+        self.a.email = 'airbnb@gmail.com'
+        self.a.password = 'bnb1'
+        self.a.first_name = 'air'
+        self.a.last_name = 'bnb'
+        self.assertEqual(self.a.to_dict()['email'], 'airbnb@gmail.com')
+        self.assertEqual(self.a.to_dict()['password'], 'bnb1')
+        self.assertEqual(self.a.to_dict()['first_name'], 'air')
+        self.assertEqual(self.a.to_dict()['last_name'], 'bnb')
+
 
 if __name__ == "__main__":
     unittest.main()

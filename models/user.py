39141f06e86790c651484da57a9b8b44859c0a29
Muhammad Aliyu Abubakar <muhammadamasanawa75@gmail.com>
2024-07-29 16:35:02 +0000

    user fix for class attr fix 4

--- models/user.py
+++ models/user.py
@@ -11,14 +11,16 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+
     def __init__(self, *args, **kwargs):
         """ Initialize instance """
-        super().__init__(self, *args, **kwargs)
-        if 'email' in kwargs.keys() and kwargs:
-            User.email = kwargs['email']
-        if 'password' in kwargs.keys() and kwargs:
-            User.password = kwargs['password']
-        if 'first_name' in kwargs.keys() and kwargs:
-            User.first_name = kwargs['first_name']
-        if 'last_name' in kwargs.keys() and kwargs:
-            User.last_name = kwargs['last_name']
+        super().__init__(**kwargs)
+        if kwargs:
+            if 'email' in kwargs.keys():
+                User.email = kwargs['email']
+            if 'password' in kwargs.keys():
+                User.pasword = kwargs['password']
+            if 'first_name' in kwargs.keys():
+                User.first_name = kwargs['first_name']
+            if 'last_name' in kwargs.keys():
+                User.email = kwargs['last_name']
